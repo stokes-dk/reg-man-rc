196cb86fdd9188615d527a0fd3e37064be25f523
Dave Stokes <stokes_dk@yahoo.ca>
2020-10-14 13:07:34 -0400

    Save minor changes

--- includes/class_lib/class-plugin-activator.php
+++ includes/class_lib/class-plugin-activator.php
@@ -4,6 +4,6 @@ use Reg_Man_RC\Model\Event;
 
 class Plugin_Activator {
 	public static function activate() {
-		error_log('Made it to Plugin_Deactivator::deactivate');
+		error_log( 'INFO: Executing ' . __CLASS__ . '::' . __FUNCTION__ );
 	} // function
 } // class
--- includes/class_lib/class-plugin-deactivator.php
+++ includes/class_lib/class-plugin-deactivator.php
@@ -3,6 +3,6 @@ namespace Reg_Man_RC;
 
 class Plugin_Deactivator {
 	public static function deactivate() {
-		error_log('Made it to Plugin_Deactivator::deactivate');
+		error_log( 'INFO: Executing ' . __CLASS__ . '::' . __FUNCTION__ );
 	} // function
 } // class
